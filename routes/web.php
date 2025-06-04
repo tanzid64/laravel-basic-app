6333260cb4d1aed5c45a2e3e69058919fedaeaa8
Tanzid Haque <tanzid3@gmail.com>
2025-06-05 00:58:39 +0600

    Home layout

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
 Route::get('/dashboard', function () {
